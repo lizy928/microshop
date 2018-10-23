eb6b3fa7a4a0ccc2448502fd59c11a86d81deb86
lizhengyuan <lzy928>
2018-10-23 20:26:41 +0800

    项目架构创建和调整依赖

---
+++ shop-pojo/src/main/java/com/shop/pojo/pojo.java
@@ -0,0 +1,19 @@
+package com.shop.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created By Lizhengyuan on 18-10-23
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class pojo {
+
+    private String id;
+    private String name;
+    private String num;
+
+}
