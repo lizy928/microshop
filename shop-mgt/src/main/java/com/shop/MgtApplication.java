05f6c45dd5c8b0e1442e71424ed136b30dd5a38f
lizhengyuan <lzy928>
2018-10-24 20:33:42 +0800

    配置文件

--- shop-mgt/src/main/java/com/shop/MgtApplication.java
+++ shop-mgt/src/main/java/com/shop/MgtApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created By Lizhengyuan on 18-10-24
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class MgtApplication {
 
     public static void main(String[] args) {
