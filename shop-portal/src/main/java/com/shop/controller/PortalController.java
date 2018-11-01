c318364dd3a9c9681ac3e966744fe62687dde82c
lizhengyuan <lzy928>
2018-11-01 20:20:35 +0800

    add portal controller

---
+++ shop-portal/src/main/java/com/shop/controller/PortalController.java
@@ -0,0 +1,20 @@
+package com.shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created By Lizhengyuan on 18-11-1
+ */
+@RestController
+@RequestMapping("portal")
+public class PortalController {
+
+    @RequestMapping("getSortMenuList")
+    public Object getSortMenuList(){
+
+        return null;
+    }
+
+
+}
